224cd5df03e880fb973142e63b2d7af6d2e43cd5
GersonPaulo <igorpaulo64@gmail.com>
2025-05-18 19:13:58 +0100

    revisao aula 1 a 7

--- MyFirst.java
+++ MyFirst.java
@@ -1,10 +1,22 @@
-import java.lang*;
 
-class MyFirst 
-{
-
-	public static void main (string arg[])
+class MyFirst
+{	public static void main(String[] args)
 	{
-		System.out.print("hello world");
+		int a;
+		a = 1;  /* ou */ int b = 23; /* ou */ int c, d, e, f;
+		for (int i = 0; i <= 3; i++)
+		{
+			c = 9*3*7*i;
+			d=c+1;
+			e =d+1;
+			f= e+1;
+			System.out.println("numero inteiro var i="+i+" c "+c+" d "+d+" e "+e+" f "+f);
+		}
+
+		System.out.println("numero inteiro var a "+ a);
+		System.out.println("numero inteiro var b "+ b);
+
+
+		System.out.println(a);
 	}
 }
